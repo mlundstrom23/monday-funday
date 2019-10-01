b4a202842fe7fe2df66356f7ad43904d191516a1
Marina Lundstrom <marina@marinas-air.worcester.local>
2019-09-30 21:43:00 -0400

    Objects in array practice

---
+++ objects-in-array.js
@@ -0,0 +1,15 @@
+let cars = [{
+    Year: 2011,
+    Make: 'Ford',
+    Model: 'Fiesta',
+},
+{   Year: 2008,
+    Make: 'Nissan',
+    Model: 'Leaf',
+},
+{   Year: 2017,
+    Make: 'Corvette',
+    Model: 'Stingray',
+}] 
+
+console.log(cars)
